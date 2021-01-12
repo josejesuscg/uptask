b14a6fe4a992727cc5dd14d5ba33e651ad59f265
josejesuscg <josejesuscg08@gmail.com>
2021-01-12 19:33:41 -0400

    proyecto finalizado

--- inc/modelos/modelo-tareas.php
+++ inc/modelos/modelo-tareas.php
@@ -2,6 +2,10 @@
 $accion = $_POST['accion'];
 $id_proyecto = (int) $_POST['id_proyecto']; // Con el int nos aseguramos que lo tome como un entero
 $tarea = $_POST['tarea'];
+$estado = $_POST['estado'];
+$id_tarea = (int) $_POST['id'];
+
+
 
 if($accion === 'crear') {
 
@@ -40,6 +44,76 @@ if($accion === 'crear') {
   
 
     
+}
+
+if($accion === 'actualizar') {
+
+      //Importar la conexion
+      include '../funciones/conexion.php';
+
+      // El try catch nos permite que en caso de que haya un error el programa siga funcionando y tambien nos devuelva un mensaje de error
+      try{
+          //Realizar la consulta a la base de datos
+         $stmt = $conn->prepare("UPDATE tareas set estado = ? WHERE id = ?");
+         $stmt->bind_param('ii', $estado, $id_tarea);
+         $stmt->execute();
+        if($stmt->affected_rows > 0){
+            $respuesta = array(
+                  'respuesta' => 'correcto',
+            );
+        } else{
+            $respuesta = array(
+                'respuesta' => 'error'
+            );
+        }
+         $stmt->close();
+         $conn->close();
+      } catch(Exception $e) {
+          //En caso de un error, tomar la exepcion
+          $respuesta = array(
+              'error' => $e->getMessage()
+          );
+      }
+  
+      echo json_encode($respuesta);
+  
+    
+    
+}
+
+if($accion === 'eliminar') {
+
+    //Importar la conexion
+    include '../funciones/conexion.php';
+
+    // El try catch nos permite que en caso de que haya un error el programa siga funcionando y tambien nos devuelva un mensaje de error
+    try{
+        //Realizar la consulta a la base de datos
+       $stmt = $conn->prepare("DELETE FROM tareas WHERE id = ?");
+       $stmt->bind_param('i', $id_tarea);
+       $stmt->execute();
+      if($stmt->affected_rows > 0){
+          $respuesta = array(
+                'respuesta' => 'correcto',
+          );
+      } else{
+          $respuesta = array(
+              'respuesta' => 'error'
+          );
+      }
+       $stmt->close();
+       $conn->close();
+    } catch(Exception $e) {
+        //En caso de un error, tomar la exepcion
+        $respuesta = array(
+            'error' => $e->getMessage()
+        );
+    }
+
+    echo json_encode($respuesta);
+
+  
+  
 }
 
 ?>
--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,20 @@ eventListeners();
 var listaProyectos = document.querySelector('ul#proyectos');
 
 function eventListeners() {
+
+    //Document Ready
+    document.addEventListener('DOMContentLoaded', function(){
+        actualizarProgreso();
+    });
+
     //Boton para crear proyecto
     document.querySelector('.crear-proyecto a').addEventListener('click', nuevoProyecto);
 
     //Boton para una nueva tarea
     document.querySelector('.nueva-tarea').addEventListener('click', agregarTarea);
+
+    //Botones para las acciones de las tareas
+    document.querySelector('.listado-pendientes').addEventListener('click', accionesTareas);
 }
 
 function nuevoProyecto(e) {
@@ -69,7 +78,7 @@ function guardarProyectoDB(nombreProyecto) {
                     //Inyectar en el HTML
                     var nuevoProyecto = document.createElement('li');
                     nuevoProyecto.innerHTML = `
-                        <a href="index.php?id_proyecto=${id_proyecto}" id="${id_proyecto}">
+                        <a href="index.php?id_proyecto=${id_proyecto}" id="proyecto:${id_proyecto}">
 
                         ${proyecto}
                     `;
@@ -165,6 +174,13 @@ function agregarTarea(e) {
                         icon: 'success'
                     })
                 }
+                //Seleccionar el parrafo con la lista vacia
+                var parrafoListaVacia = document.querySelectorAll('.lista-vacia') //querySelectorAll regresa un segundo parametro que se llama length
+                if(parrafoListaVacia.length > 0 ){
+                    document.querySelector('.lista-vacia').remove()
+                }
+
+
                 //Construir el template
                 var nuevaTarea = document.createElement('li');
 
@@ -190,6 +206,9 @@ function agregarTarea(e) {
 
                 //Limpiar formulario 
                 document.querySelector('.agregar-tarea').reset();
+                
+                //Actualizar el progreso
+                actualizarProgreso();
             }else {
                 //Hubo un error
                 Swal.fire({
@@ -199,10 +218,157 @@ function agregarTarea(e) {
                     icon: 'error'
                 })
             }
+            
         }
     }
-
         //Enviar la consulta
         xhr.send(datos);
     }
+}
+// Cambia el estado de las tareas o las elimina
+function accionesTareas(e) {
+    e.preventDefault();
+
+    // console.log('click en listado');
+    // console.log(e.target)//Tengo acceso a que especificamente el usuario le esta dando click, se le conoce como delegation
+
+    if(e.target.classList.contains('fa-check-circle')){
+        if(e.target.classList.contains('completo')){
+            e.target.classList.remove('completo')
+            cambiarEstadoTarea(e.target, 0);
+        } else {
+            e.target.classList.add('completo');
+            cambiarEstadoTarea(e.target, 1);
+        }
+    } 
+
+    if(e.target.classList.contains('fa-trash')){
+        Swal.fire({
+            title: 'Estas seguro(a)?',
+            text: "Esta accion no puede deshacerse!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, borrar!',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                var tareaEliminar = e.target.parentElement.parentElement;
+                //Borrar de la BD
+                eliminarTareaBD(tareaEliminar);
+
+                //Borrar del HTML
+                tareaEliminar.remove()
+              Swal.fire(
+                'Eliminado!',
+                'Esta tarea ha sido eliminada.',
+                'success'
+              )
+            }
+          })
+    } 
+}
+
+//Completa o descompleta una tarea
+function cambiarEstadoTarea(tarea, estado) {
+    var idTarea = tarea.parentElement.parentElement.id.split(':');
+    //2- console.log(idTarea[1]); //Se coloca el 1 en corchetes porque es la posicion con la que se accede al ID en la consola del navegador 
+    //1- console.log(tarea.parentElement.parentElement.id.split(':')); //ParentElement nos permite ir del hijo al padre en JS /// split te separa lo que le coloques en los parametros, es decir al estar en esos ':' el separara
+
+    //Crear llamado a Ajax
+    var xhr = new XMLHttpRequest();
+
+    //Informacion
+    var datos = new FormData();
+    datos.append('id', idTarea[1]);
+    datos.append('accion', 'actualizar');
+    datos.append('estado', estado);
+
+    
+
+    //Abrir la conexion 
+    xhr.open('POST', 'inc/modelos/modelo-tareas.php', true);
+
+    //On load 
+    xhr.onload = function() {
+        if(this.status == 200) {
+            console.log(JSON.parse(xhr.responseText));
+            //Actualizar el progreso
+            actualizarProgreso();
+        }
+    }
+
+    //Enviar la peticion 
+    xhr.send(datos);
+}
+
+//Elimina las tareas de las BD
+function eliminarTareaBD(tarea) {
+    var idTarea = tarea.id.split(':');
+    
+    //Crear llamado a Ajax
+    var xhr = new XMLHttpRequest();
+
+    //Informacion
+    var datos = new FormData();
+    datos.append('id', idTarea[1]);
+    datos.append('accion', 'eliminar');
+
+    
+
+    //Abrir la conexion 
+    xhr.open('POST', 'inc/modelos/modelo-tareas.php', true);
+
+    //On load 
+    xhr.onload = function() {
+        if(this.status == 200) {
+            console.log(JSON.parse(xhr.responseText));
+
+            //Comprobar que haya tareas restantes
+            var listaTareasRestantes = document.querySelectorAll('li.tarea');
+            if(listaTareasRestantes.length === 0 ){
+                document.querySelector('.listado-pendientes ul').innerHTML = "<p class='lista-vacia'>No hay tareas en este proyecto</p>"
+            }
+            //Actualizar el progreso
+            actualizarProgreso();
+
+        }
+    }
+
+    //Enviar la peticion 
+    xhr.send(datos);
+}
+
+//Actualiza el avance del proyecto
+function actualizarProgreso(){
+    const tareas = document.querySelectorAll('li.tarea');
+
+    //Obtener las tareas completadas
+    const tareasCompletadas = document.querySelectorAll('i.completo')
+
+    //Determinar el avance
+    const avance = Math.round((tareasCompletadas.length / tareas.length) * 100);
+    //Math.round redondea los numeros de la operacion que se esta ejecutando, es decir quita decimales 
+
+    //Asignar el porcentaje a la barra
+    const porcentaje = document.querySelector('#porcentaje');
+    porcentaje.style.width = avance+'%'; // style.width nos permite cambiar el ancho. avance lo requerimos como porcentaje lo cual hay que utilizar el mas y concatenar con el %
+
+
+    //Mostrar alerta de proyecto terminado al 100
+    if(avance === 100 ){
+        Swal.fire({
+            title: 'Proyecto Terminado',
+            text: 'No tienes tareas pendientes!',
+            showConfirmButton: true,
+            icon: 'success'
+        })
+    } 
+    
+    
+    // console.log(avance)
+    // console.log(tareas.length); //Nos trae a la consola cuantos elementos tenemos de tareas
+
+    
 }
